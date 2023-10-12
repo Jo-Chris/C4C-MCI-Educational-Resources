12571341f722b9c921255ef2ad6ce7b50692e076
Jo-Chris <christof.jori.dev@gmail.com>
2023-10-12 15:45:06 +0200

    t

--- contracts/MyAwesomeToken.sol
+++ contracts/MyAwesomeToken.sol
@@ -2,9 +2,6 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-// Beispielcode zur Verwendung in Remix
-// https://remix.ethereum.org/
-
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyAwesomeToken is ERC20 {
